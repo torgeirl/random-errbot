46fce018aace7b840c108d99a9c1fe332ade0e41
Torgeir Lebesbye <torgeir@lebesbye.com>
2021-06-27 12:01:27 +0200

    forgot to generate the actual result :facepalm:

--- test_random.py
+++ test_random.py
@@ -12,6 +12,8 @@ class TestRandom(object):
         assert(result == 'HEADS!' or result == 'TAILS!')
 
     def test_roll(self, testbot):
+        testbot.push_message('!roll')
+        result = testbot.pop_message()
         assert(result == '1' or result == '2' or result == '3' or \
                result == '4' or result == '5' or result == '6')
 
